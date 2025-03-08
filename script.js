037b7bae905f05213fee4542ab2f1bbf059a89d0
Shivam kumar <146973981+Shivam4401@users.noreply.github.com>
2025-03-08 20:46:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+document.getElementById("loginform").addEventListener("submit",(e)=>{
+	e.preventDefault();
+	const username = document.getElementById("username").value;
+	const password = document.getElementById("password").value;
+	const rememberme = doucument.getElementById("checkbox");
+	alert("Logged in as " , username);
+	if(rememberme.checked){
+		localStorage.setItem("username", username);
+		localStorage.setItem("password", password);
+	}
+	else{
+		localStorage.removeItem("username", username);
+		localStorage.removeItem("password", password);
+	}
+});
+
+window.onload = function() {
+ const username = localStorage.getItem("username");
+ const password = localStorage.getItem("password");
+	if(username && password){
+		document.getElementById("existing").style.display = "block";
+	}
+};
+
+document.getElementById("existing").addEventListener("click",()=>{
+	const username = localStorage.getItem('username');
+	alert("Logged in as ",username);
+})
+
